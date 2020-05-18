dc42147c1130dbae7118c77371a1486028dd71ee
Fernando Elger <fee.elger@gmail.com>
2020-05-17 21:50:34 -0300

    [New] add delete and validateID function

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const {uuid, isUuid} = require("uuidv4");
+const {uuid, isUuid} = require("uuidv4"); // Universal Unique ID 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-const repositories = [];
-
 
 function logRequest(request, response, next){
   const {method, url} = request;
@@ -20,7 +17,20 @@ function logRequest(request, response, next){
   console.timeEnd(logLabel);
 }
 
+function validateRepositoryID(request, response, next){
+  const {id} = request.params;
+
+  if (!isUuid(id)){
+    return response.status(400).json({error : 'Invalid Repository ID.'});
+  }
+
+  return next();
+}
+
 app.use(logRequest);
+app.use("/repositories/:id", validateRepositoryID);
+
+const repositories = [];
 
 app.get("/repositories", (request, response) => {
   const {title} = request.query;
@@ -58,7 +68,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
