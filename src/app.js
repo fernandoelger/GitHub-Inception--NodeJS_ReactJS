4f1639ad4f80fc0d47a755cadb2308a07d86746b
Fernando Elger <fee.elger@gmail.com>
2020-05-19 00:32:32 -0300

    [New] add repository likes route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// print Method, Url and duration time in each request
 function logRequest(request, response, next){
   const {method, url} = request;
 
@@ -20,6 +21,7 @@ function logRequest(request, response, next){
 function validateRepositoryID(request, response, next){
   const {id} = request.params;
 
+  // Verify if the request ID is in uuid format
   if (!isUuid(id)){
     return response.status(400).json({error : 'Invalid Repository ID.'});
   }
@@ -35,6 +37,8 @@ const repositories = [];
 app.get("/repositories", (request, response) => {
   const {title} = request.query;
 
+  // If title is not empty, return all repositories that contains title
+  // Else return all repositories
   const results = title
     ? repositories.filter(repository => repository.title.includes(title))
     : repositories;
@@ -45,7 +49,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const {title, url, techs} = request.body;
 
-  const repository = {id: uuid(), title, url, techs};
+  const repository = {id: uuid(), title, url, techs, likes: 0};
   repositories.push(repository);
 
   return response.status(201).json(repository);
@@ -81,8 +85,20 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
+// POST method because now we have two entities: repository and likes
+// So we can create differents business rules for each one
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex < 0){
+    return response.status(400).json({error: 'Repository not found.'});
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.status(204).send();
 });
 
 module.exports = app;
