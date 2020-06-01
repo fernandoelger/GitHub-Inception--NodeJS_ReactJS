90208d52d44d2fb548999048efd6637fa33a9160
Fernando Elger <fee.elger@gmail.com>
2020-06-01 05:24:22 -0300

    [Test] fix likes test

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -6,8 +6,8 @@ describe("Likes", () => {
     const repository = await request(app)
       .post("/repositories")
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
+        url: "https://github.com/fernandoelger/GitHub-Inception--NodeJS",
+        title: "GitHub Inception",
         techs: ["Node", "Express", "TypeScript"]
       });
 
